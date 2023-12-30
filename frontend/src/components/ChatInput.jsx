fb6a0cfd49d57b8293783d38db9ccb81c89c1a81
SouravCodes1080 <107361153+souravcodes1080@users.noreply.github.com>
2023-12-30 23:29:55 +0530

    message box workin

--- frontend/src/components/ChatInput.jsx
+++ frontend/src/components/ChatInput.jsx
@@ -1,39 +1,59 @@
-import React, { useState } from 'react'
-import Picker from "emoji-picker-react"
-import smiley2 from "../assets/smiley2.svg"
-import send from "../assets/send.svg"
+import React, { useState } from "react";
+import Picker from "emoji-picker-react";
+import smiley2 from "../assets/smiley2.svg";
+import send from "../assets/send.svg";
 import "../styles/chat.css";
 
-export default function ChatInput() {
-    const [showEmojiPicker, setShowEmojiPicker] = useState(false)
-    const [msg, setMsg] = useState("")
+export default function ChatInput({ handleChatMsg }) {
+  const [showEmojiPicker, setShowEmojiPicker] = useState(false);
+  const [msg, setMsg] = useState("");
 
-    const handleEmojiPicker = () =>{
-        setShowEmojiPicker(!showEmojiPicker)
+  const handleEmojiPicker = () => {
+    setShowEmojiPicker(!showEmojiPicker);
+  };
+
+  const handleEmojiClick = (emojiObject, event) => {
+    const emoji = emojiObject.emoji;
+    const newMessage = msg + emoji;
+    setMsg((prevMsg) => prevMsg + emoji);
+  };
+
+  const sendChat = (e) => {
+    console.log(e);
+    e.preventDefault();
+    if (msg.length > 0) {
+      handleChatMsg(msg);
+      setMsg("");
+      console.log(msg)
     }
-
-    const handleEmojiClick = (emoji) => {
-        let message = msg + emoji.emoji;
-        setMsg(message);
-    };
-    
+  };
   return (
-    <> 
-    {showEmojiPicker && <Picker className='emoji-picker-react' onEmojiClick={handleEmojiClick}/>}
-        <div className="input-wrapper">
-            <div className="emoji" onClick={handleEmojiPicker}>
-                <img src={smiley2} alt="" width={"30px"}/>
-               
-            </div>
-            <div className="input-tab">
-                <form>
-                    <input type="text" placeholder='Type your message here...' value={msg} onChange={(e)=>setMsg(e.target.value)}/>
-                </form>
-            </div>
-            <div className="send">
-            <img src={send} alt="" width={"30px"}/>
-            </div>
+    <>
+      {showEmojiPicker && (
+        <Picker
+          className="emoji-picker-react"
+          onEmojiClick={handleEmojiClick}
+        />
+      )}
+      <div className="input-wrapper">
+        <div className="emoji" onClick={handleEmojiPicker}>
+          <img src={smiley2} alt="" width={"30px"} />
         </div>
+
+        <form onSubmit={(e) => sendChat(e)} className="input-form">
+          
+            <input
+              type="text"
+              placeholder="Type your message here..."
+              value={msg}
+              onChange={(e) => setMsg(e.target.value)}
+            />
+          
+          <button className="send" type="submit">
+            <img src={send} alt="" width={"30px"} />
+          </button>
+        </form>
+      </div>
     </>
-  )
+  );
 }
