936764037136dfb68454d4209eea5ef76d748e99
SouravCodes1080 <107361153+souravcodes1080@users.noreply.github.com>
2024-08-17 15:23:58 +0530

    cors origin fix

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,7 +11,7 @@ const server = app.listen(process.env.PORT, () => {
 
 const io = new Server(server, {
     cors: {
-      origin: "http://localhost:5173",
+      origin: "https://talk-mixer-frontend.onrender.com/",
       credentials: true,
     },
   });
